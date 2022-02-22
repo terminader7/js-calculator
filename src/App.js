757a4e6ecb5d9b3debfe79d62baa13819e4ccfb3
Nader Ebrahim~ <nebrahim2424@gmail.com>
2022-02-22 14:52:51 -0800

    small styling tweaks

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import "./App.css";
 import styled from "styled-components";
 import { Calculator } from "./components/Calculator";
 
+const AppPage = styled.div`
+  background-color: lavender;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+`;
 function App() {
   return (
-    <div className="App">
+    <AppPage>
       <Calculator />
-    </div>
+    </AppPage>
   );
 }
 
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,7 +4,14 @@ import styled from "styled-components";
 import { CalculatorNumPad } from "./CalculatorNumPad";
 import { CalculatorDisplay } from "./CalculatorDisplay";
 
-const CalculatorContainer = styled.div``;
+const CalculatorContainer = styled.div`
+  width: 340px;
+  height: 540px;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #485461;
+  background-image: linear-gradient(315deg, #485461 0%, #28313b 74%);
+`;
 export const Calculator = () => {
   const [display, setDisplay] = useState("");
 
