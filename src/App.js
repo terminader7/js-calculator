11272e362296155cf3b328d2483bd104fd66cbbf
Nader Ebrahim~ <nebrahim2424@gmail.com>
2022-02-22 10:35:27 -0800

    set skeleton for app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: green;
+  font-family: helvetica;
+`;
+
+const SecondaryTitle = styled(Title)`
+  color: red;
+  display: flex;
+`;
 
 function App() {
   return (
     <div className="App">
-      <h1>no.1 heimerdinger NA</h1>
+      <Title>no.1 heimerdinger NA</Title>
+      <SecondaryTitle>Angular is bad lel</SecondaryTitle>
     </div>
   );
 }
